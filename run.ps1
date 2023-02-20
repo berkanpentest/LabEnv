ea9c564f6372fa11d544604d9c6de4268820748f
berkanpentest <125474668+berkanpentest@users.noreply.github.com>
2023-02-20 11:02:50 +0300

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,6 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -URI https://raw.githubusercontent.com/wazehell/vulnerable-AD/master/vulnad.ps1 -OutFile C:/vulnad.ps1
+
+Import-Module C:\vulnad.ps1
+Sleep 5
+Invoke-VulnAD -UsersLimit 50 -DomainName acme.local
+
